85e83615f93143f75266e579bc794298f3a9dafc
oDevilo <15757166470@163.com>
2016-12-06 10:08:51 +0800

    requirejs

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Front-end Web
+
+
+## AngularJS
+
+## bootstrap
+
+## charts(各种图形插件)
+
+## Game
+
+## requirejs(JavaScript模块载入框架)
---
+++ requirejs/main.js
@@ -0,0 +1,23 @@
+/*  
+	require.config是用来配置模块加载位置，
+ 	简单点说就是给模块起一个更短更好记的名字，
+ 	比如将百度的jquery库地址标记为jquery，
+ 	这样在require时只需要写["jquery"]就可以加载该js
+	
+	paths还有一个重要的功能，就是可以配置多个路径，
+	如果远程cdn库没有加载成功，可以加载本地的文件
+ */
+ 
+
+require.config({
+	paths : {
+		"jquery" : ["http://libs.baidu.com/jquery/2.0.3/jquery", "jquery"],
+		"b" : "b"
+	}
+});
+
+/*在这里配置require，那么别的地方只需要加载此文件，省去了很多配置*/
+
+require(["jquery","b"] , function(){
+	alert("所有js加载完成");
+});
