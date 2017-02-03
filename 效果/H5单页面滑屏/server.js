0f5e37142c37eb2cf05c9217049cad1ebbba2860
oDevilo <15757166470@163.com>
2017-02-03 13:57:29 +0800

    H5滑动

---
+++ 效果/H5单页面滑屏/server.js
@@ -0,0 +1,12 @@
+// 引入模块 npm install socket.io下载socket.io
+var http = require('http')
+	express = require("express");
+
+app = express();
+// 创建一个服务器
+server = http.createServer(app);
+
+app.use('/', express.static(__dirname));
+console.log("应用路径：" + __dirname);
+// 监听8080端口
+server.listen(8888);
